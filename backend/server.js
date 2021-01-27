a17c779763757be0a7e68f07c1b9e21bbebb2f8a
Alex Shvetsov <alexsh1412@gmail.com>
2021-01-27 16:53:04 +0200

    version 1.0

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,15 @@ const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/fronted/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'fronted', 'build', 'index.html'))
-} else {
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'fronted', 'build', 'index.html'))
+    )
+  } else {
     app.get('/', (req, res) => {
-        res.send('API is running...')
+      res.send('API is running....')
     })
-}
-
+  }
 
 
 
